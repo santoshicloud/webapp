19246fd42d61faf7271d26b2db43551a96cd012d
Santoshi Manvita Karuturi <karuturi.sa@northeastern.edu>
2024-02-17 17:23:04 -0500

    Integration Testing Case 3

--- models/userModel.js
+++ models/userModel.js
@@ -14,12 +14,6 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, pr
   define: {
     underscored: true, // Use snake_case for automatically generated attributes (e.g., createdAt, updatedAt)
   },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-    }
-  },
 });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
