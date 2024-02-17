b6490b42b3c22efda9a31cc29261c2eba193085e
Santoshi Manvita Karuturi <karuturi.sa@northeastern.edu>
2024-02-17 12:14:18 -0500

    test 5

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ describe('User Endpoint Integration Tests', () => {
     this.timeout(10000);
 
     const userData = {
-      email: 'yadav111@example.com',
+      email: 'yadav1112@example.com',
       password: 'TestPassword123',
       firstName: 'John',
       lastName: 'Doe'
