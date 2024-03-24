2c6b78e523328c7bd2227a0b85794c892120ce94
Santoshi Manvita Karuturi <karuturi.sa@northeastern.edu>
2024-03-24 02:36:54 -0400

    t1

--- app.js
+++ app.js
@@ -7,16 +7,6 @@ const userRoutes = require('./routes/userRoutes');
 const { sequelize } = require('./models/userModel');
 const logger = require('./logger');
 
-// Setting up Winston logger
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ],
-});
-
 // If we're not in production then log to the `console` with the format:
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
