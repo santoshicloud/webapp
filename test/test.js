f1fd19533f1ca58c2e5f3c4eb6d2c2a9e1594939
Santoshi Manvita Karuturi <karuturi.sa@northeastern.edu>
2024-04-16 12:12:06 -0400

    testing v1

--- test/test.js
+++ test/test.js
@@ -82,7 +82,7 @@ describe('User API Integration Tests', () => {
     it('should create an account and validate account creation', async () => {
         // Create user
         const createResponse = await request(app)
-            .post('/v2/user')
+            .post('/v1/user')
             .send(userCredentials);
         expect(createResponse.statusCode).toBe(201);
         expect(createResponse.body).toHaveProperty('id');
@@ -90,7 +90,7 @@ describe('User API Integration Tests', () => {
 
         // Validate account creation
         const validateResponse = await request(app)
-            .get('/v2/user/self')
+            .get('/v1/user/self')
             .auth(userCredentials.email, userCredentials.password); // Basic Auth
         expect(validateResponse.statusCode).toBe(200);
         expect(validateResponse.body.id).toBe(createdUserId);
@@ -105,14 +105,14 @@ describe('User API Integration Tests', () => {
 
         // Update user
         const updateResponse = await request(app)
-            .put('/v2/user/self')
+            .put('/v1/user/self')
             .auth(userCredentials.email, userCredentials.password) // Basic Auth
             .send(updatedUser);
         expect(updateResponse.statusCode).toBe(204);
 
         // Validate account update
         const validateUpdateResponse = await request(app)
-            .get('/v2/user/self')
+            .get('/v1/user/self')
             .auth(userCredentials.email, userCredentials.password); // Basic Auth
         expect(validateUpdateResponse.statusCode).toBe(200);
         expect(validateUpdateResponse.body.firstName).toBe(updatedUser.firstName);
